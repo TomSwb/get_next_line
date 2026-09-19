81487b13a527a8831fe02c6aaa08b63e26e00c85
Thomas Schwab <thomasschwab@bitminded.ch>
2026-09-19 22:56:25 +0200

    Update get_next_line.c

--- program_version/get_next_line.c
+++ program_version/get_next_line.c
@@ -14,20 +14,13 @@
 
 char	*get_next_line(int fd)
 {
-	char		*line;
-	static char	*lftvr; 
-	size_t		len;
-	int			reading;
-
-	line = malloc(BUFFER_SIZE + 1);
-	if (!line)
-		return (NULL);
-	reading = read(fd, line, BUFFER_SIZE);
-	if (reading == -1)
-	{
-		free(line);
-		return (NULL);
-	}
-	line[reading + 1] = '\0';
-	return (line);
+		char		*line;
+		static char	*lftvr; 
+		size_t		len;
+	
+		line = ft_extract_buffer(fd);
+		if (!line)
+			return (NULL);
+		
+		return (line);
 }
