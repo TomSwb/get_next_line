6e32ea648684c48524182f97bd5d3466dd9b4dff
Thomas Schwab <thomasschwab@bitminded.ch>
2026-09-21 22:28:27 +0200

    Update get_next_line.c

--- program_version/get_next_line.c
+++ program_version/get_next_line.c
@@ -25,7 +25,7 @@ char	*get_next_line(int fd)
         }
         while (ft_strchr(data, '\n') != 0 
                 || ft_strlen(data) < BUFFER_SIZE)
-            data = ft_strcat_buffer(data, fd);
+            data = ft_extract_buffer(data, fd);
         ft_extract_line(line, data);
 		return (line);
 }
