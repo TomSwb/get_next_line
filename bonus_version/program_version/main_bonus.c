511f8f75e36a5812e9f6654471ad620d3586e839
Thomas Schwab <thomasschwab@bitminded.ch>
2026-09-22 20:45:51 +0200

    Update main_bonus.c

--- bonus_version/program_version/main_bonus.c
+++ bonus_version/program_version/main_bonus.c
@@ -73,12 +73,40 @@ void	many_files_gnl(char **av)
 {
 	int *fds;
 	size_t len;
+	size_t i;
+	size_t line_len;
 	
 	fds = allocate_fds(av, &len);
+	if (!fds)
+		return ;
+	i = 0;
 	while (check_fds(fds, len) == 1)
 	{
-	...
+		i = 0;
+		while (i < len)
+		{
+			if (fds[i] != -1)
+			{
+				line = get_next_line(fds[i]);
+				if (line != NULL)
+				{
+					line_len = find_c_index(line, '\0');
+					write(1, line, line_len);
+					free(line);
+					read(0, &c, 1);
+					while (c != '\n')
+						read(0, &c, 1);
+				}
+				else
+				{
+					close(fds[i]);
+					fds[i] = -1;
+				}
+			}
+			i++;
+		}
 	}
+	free(fds);
 }
 
 int *allocate_fds(char **av, size_t *i)
